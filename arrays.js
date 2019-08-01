cd809800c5e3162493ee1626fb88032a6073e728
Dominic <dominic.m.chu@gmail.com>
2019-08-01 09:29:09 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -23,3 +23,7 @@ function destructivelyAddElementToEndOfArray(array, element) {
 function accessElementInArray(array, index) {
   return array[index];
 }
+
+function destrutivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift();
+}
