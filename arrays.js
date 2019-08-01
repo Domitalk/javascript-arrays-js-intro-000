b68e1626e0ca131af38037312b725c2892634b58
Dominic <dominic.m.chu@gmail.com>
2019-08-01 08:55:57 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -6,6 +6,7 @@ function addElementToBeginningOfArray(array, element) {
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
+  var secondArray = [array]
   secondArray.unshift(element);
   return secondArray;
 }
