9e04cc502558e5d7965f32e2b906f8d5d728ca80
Dominic <dominic.m.chu@gmail.com>
2019-08-01 09:22:27 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -21,6 +21,5 @@ function destructivelyAddElementToEndOfArray(array, element) {
 }
 
 function accessElementInArray(array, index) {
-  var thirdArray = [array];
-  return thirdArray[index];
+  return array[index];
 }
