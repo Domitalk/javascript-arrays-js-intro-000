f08cde9232aecadb22f2a3ffa4514e9f85242e82
Dominic <dominic.m.chu@gmail.com>
2019-08-01 08:54:24 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,9 +1,8 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(array, element) {
-  var firstArray = [array];
-  var secondArray = [element, ...array];
-  return secondArray;
+  var firstArray = [element, ...array];
+  return firstArray;
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
