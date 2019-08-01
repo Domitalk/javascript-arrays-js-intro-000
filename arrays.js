5f0f8f6a0da21496c10dcb4915c1004f289ee77f
Dominic <dominic.m.chu@gmail.com>
2019-08-01 08:48:55 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -8,6 +8,6 @@ function addElementToBeginningOfArray(array, element) {
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
   var thirdArray = [array];
-  thirdArray[1] = element;
+  var thirdArray = [element, ...array];
   return thirdArray;
 }
