cb13449758ff4441591fe14149d9d6df1218470a
Dominic <dominic.m.chu@gmail.com>
2019-08-01 09:30:10 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -24,7 +24,6 @@ function accessElementInArray(array, index) {
   return array[index];
 }
 
-function destrutivelyRemoveElementFromBeginningOfArray(array) {
+function destructivelyRemoveElementFromBeginningOfArray(array) {
   array.shift();
-  return array;
 }
